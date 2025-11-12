08c3e674e131d6e462a32f92647f681f2cff6672
Ragaeeb Haq <7464039+ragaeeb@users.noreply.github.com>
2025-11-12 01:43:02 -0500

    Remove unused external dependency

--- tsdown.config.ts
+++ tsdown.config.ts
@@ -4,7 +4,6 @@ export default defineConfig({
     clean: true,
     dts: true,
     entry: ['src/index.ts'],
-    external: ['node:timers/promises'],
     format: ['esm'],
     minify: true,
     platform: 'neutral',
